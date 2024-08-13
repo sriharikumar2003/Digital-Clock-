db8b19678dd4e094ce2870f6313b2c3c1bb2b6a3
Sri Hari Kumar P <sriharikumar.0903@gmail.com>
2024-08-13 15:03:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function updateTime(){
+    const time  = new Date();
+    let hours = time.getHours().toString().padStart(2,0);
+    const meridian = (hours < 12)?"AM":"PM";
+    hours = hours%12;
+    const minutes = time.getMinutes().toString().padStart(2,0);;
+    const seconds = time.getSeconds().toString().padStart(2,0);;
+    document.getElementById("clock").textContent = `${hours}:${minutes}:${seconds}:${meridian}`;
+}
+
+setInterval(updateTime,1000);
