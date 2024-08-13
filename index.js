590796cf93b133c493506d796258b83bc1de3b7e
Sri Hari Kumar P <sriharikumar.0903@gmail.com>
2024-08-13 15:10:01 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function updateTime(){
     const time  = new Date();
     let hours = time.getHours().toString().padStart(2,0);
     const meridian = (hours < 12)?"AM":"PM";
-    hours = hours%12;
+    hours = hours%12
+    hours = hours.toString().padStart(2,0);
     const minutes = time.getMinutes().toString().padStart(2,0);;
     const seconds = time.getSeconds().toString().padStart(2,0);;
     document.getElementById("clock").textContent = `${hours}:${minutes}:${seconds}:${meridian}`;
